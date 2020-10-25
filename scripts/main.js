3035cf39a557c76b664c30e93b485234aae67971
George <georgezhu211@gmail.com>
2020-10-25 17:27:12 -0500

    add function for each buttons

--- scripts/main.js
+++ scripts/main.js
@@ -5,17 +5,59 @@ let divide = (a, b) => a / b;
 
 function operate(operator, op1, op2) {
     switch(operator) {
-        case 'add':
+        case '+':
             return add(op1, op2);
             break;
-        case 'subtract':
+        case '-':
             return subtract(op1, op2);
             break;
-        case 'multiply':
+        case 'x':
             return multiply(op1, op2);
             break;
-        case 'divide':
+        case '÷':
             return divide(op1, op2);
             break;
     }
 }
+
+const display = document.querySelector('.display');
+const numBtns = document.querySelectorAll('.num-btn');
+const opBtns = document.querySelectorAll('.op-btn');
+const calcBtn = document.querySelector('.calc-btn');
+const clearBtn = document.querySelector('.clear-btn');
+
+let a = '';
+let b = '';
+let operator = '';
+
+numBtns.forEach((button) => {
+    button.addEventListener('click', () => {
+        display.textContent += button.id;
+    });
+});
+
+opBtns.forEach((button) => {
+    button.addEventListener('click', () => {
+        a = Number(display.textContent);
+        display.textContent += button.id;
+        operator = button.id;
+    });
+});
+
+calcBtn.addEventListener('click', () => {
+    string = display.textContent;
+    opArray = ['+','-','x','÷'];
+    opArray.forEach((operator) => {
+        let index = string.indexOf(operator);
+        if (index > 0) b = Number(string.slice(index + 1));
+    });
+    display.textContent = operate(operator, a, b);
+});
+
+clearBtn.addEventListener('click', () => {
+    display.textContent = '0';
+    a = '';
+    b = '';
+    operator = '';
+});
+
